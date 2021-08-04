67e611a4829ed1a94432df760289a7b796e217dc
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 12:02:23 +0800

    [JavaCollection][Juan Carlos Salvador] feat: added features for stream filter

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -3,19 +3,28 @@ package com.thoughtworks.collection;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamFilter {
 
     public List<Integer> filterEven(List<Integer> numbers) {
-        throw new NotImplementedException();
+
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .collect(Collectors.toList());
+
     }
 
     public List<String> filterEndWithS(List<String> words) {
-        throw new NotImplementedException();
+        return words.stream()
+                .filter(word -> word.charAt(word.length() - 1) == 's')
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
-        throw new NotImplementedException();
+        return numbers.stream()
+                .filter(anotherNumbers::contains)
+                .collect(Collectors.toList());
     }
 
 
